e7334b90720faa8e59e93ffe472f920e547691be
seunghee63 <0603yang@naver.com>
2019-08-06 05:37:07 +0900

    Modify review_listview using databinding

--- app/src/main/java/com/song2/boostcourse/data/ReviewData.java
+++ app/src/main/java/com/song2/boostcourse/data/ReviewData.java
@@ -7,4 +7,64 @@ public class ReviewData {
     public String comment;
     public float rate;
     public String like;
+
+    public String getProfile_img() {
+        return profile_img;
+    }
+
+    public void setProfile_img(String profile_img) {
+        this.profile_img = profile_img;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getLike() {
+        return like;
+    }
+
+    public void setLike(String like) {
+        this.like = like;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewData{" +
+                "profile_img='" + profile_img + '\'' +
+                ", user_id='" + user_id + '\'' +
+                ", date='" + date + '\'' +
+                ", rate=" + rate +
+                ", comment='" + comment + '\'' +
+                ", like=" + like +
+                '}';
+    }
 }
--- app/src/main/java/com/song2/boostcourse/util/ReviewAdapter.java
+++ app/src/main/java/com/song2/boostcourse/util/ReviewAdapter.java
@@ -1,6 +1,8 @@
 package com.song2.boostcourse.util;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,16 +11,17 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import de.hdodenhof.circleimageview.CircleImageView;
+
 import com.song2.boostcourse.R;
 import com.song2.boostcourse.data.ReviewData;
-
+import com.song2.boostcourse.databinding.ItemReviewBinding;
 
 import java.util.ArrayList;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ReviewAdapter extends BaseAdapter {
-    private LayoutInflater inflater = null;
+    private LayoutInflater inflater;
     private ArrayList<ReviewData> ReviewList;
     private int nListCnt = 0;
 
@@ -45,27 +48,50 @@ public class ReviewAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
 
+        ItemReviewBinding binding;
+
+        //item lyout을 객체로 받아
+        inflater = LayoutInflater.from(viewGroup.getContext());
+        binding = DataBindingUtil.getBinding(view);
+
+        if(binding == null){
+            binding = DataBindingUtil.inflate(inflater, R.layout.item_review, viewGroup, false);
+        }
+
+/*      //non using dataBinding
         Context context = viewGroup.getContext();
 
         if (view == null) {
-            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            if (inflater == null)
+            {
+                inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
+
         }
+
         view = inflater.inflate(R.layout.item_review, viewGroup, false);
+
 
         TextView user_id =  view.findViewById(R.id.tv_review_item_user_id);
         TextView review_date = view.findViewById(R.id.tv_review_item_date);
         TextView comment = view.findViewById(R.id.tv_review_item_contents);
         TextView like_cnt = view.findViewById(R.id.tv_review_item_like_cnt);
-        CircleImageView profile_img = view.findViewById(R.id.cv_review_item_profile_img);
         RatingBar rating = view.findViewById(R.id.rating_bar_review_item_review_rate);
+        CircleImageView profile_img = view.findViewById(R.id.cv_review_item_profile_img);
 
-        //Glide.with(view).load(ReviewList.get(position).profile_img).into(profile_img);
         rating.setRating(ReviewList.get(position).rate);
         user_id.setText(ReviewList.get(position).user_id);
         review_date.setText(ReviewList.get(position).date);
         comment.setText(ReviewList.get(position).comment);
         like_cnt.setText(ReviewList.get(position).like);
+        //Glide.with(view).load(ReviewList.get(position).profile_img).into(profile_img);*/
 
-        return view;
+        //return  view;
+
+        binding.setReviewData((ReviewData)getItem(position));
+        binding.executePendingBindings();
+
+        return binding.getRoot();
     }
 }
