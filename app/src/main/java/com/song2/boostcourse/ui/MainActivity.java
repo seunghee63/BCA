1c7b74071197318ab48a29d9e28dab153bd48110
seunghee63 <0603yang@naver.com>
2019-08-06 03:46:52 +0900

    Add reveiw listView

--- app/src/main/java/com/song2/boostcourse/ui/MainActivity.java
+++ app/src/main/java/com/song2/boostcourse/ui/MainActivity.java
@@ -4,11 +4,16 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.song2.boostcourse.R;
 import com.song2.boostcourse.data.MovieInfo;
+import com.song2.boostcourse.data.ReviewData;
 import com.song2.boostcourse.databinding.ActivityMainBinding;
+import com.song2.boostcourse.util.ReviewAdapter;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         binding.setActivity(this);
         binding.setMovieInfo(new MovieInfo(15,2));
 
-        //binding.listViewMainActReviewList;
+        ListView listViewMainActReviewList = binding.listViewMainActReviewList;
 
+        settingDataSet(listViewMainActReviewList);
     }
 
     public void clickThumpUpBtn(View view) {
@@ -64,5 +70,34 @@ public class MainActivity extends AppCompatActivity {
             binding.ivMainActThumbDown.setSelected(!binding.ivMainActThumbDown.isSelected());
             binding.getMovieInfo().onClickThumbDown(binding.ivMainActThumbDown.isSelected());
         }
+    }
+
+    public void settingDataSet(ListView reviewList){
+
+        ArrayList<ReviewData> data = new ArrayList<>();
+
+        ReviewData oneData = new ReviewData();
+        ReviewData twoData = new ReviewData();
+
+        oneData.profile_img = "tmpImg";
+        oneData.user_id = "song2";
+        oneData.date = "3시간전";
+        oneData.comment = "이렇게 흥미로운 영화는 오랜만이에요!";
+        oneData.rate = 4;
+        oneData.like = "좋아요  "+3;
+
+        twoData.profile_img = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwinndibrezjAhXaa94KHR8iAtkQjRx6BAgBEAU&url=http%3A%2F%2Fsocksplus.net%2Fproduct%2Fdetail.html%3Fproduct_no%3D13507&psig=AOvVaw2DZWjPEfCHSHcxbpnpF6CM&ust=1565115898969108";
+        twoData.user_id = "수면양말";
+        twoData.date = "어제";
+        twoData.comment = "지루해 죽는줄;;;";
+        twoData.rate = 0;
+        twoData.like = "좋아요  "+0;
+
+        data.add(oneData);
+        data.add(twoData);
+        data.add(oneData);
+
+        final ReviewAdapter reviewAdapter = new ReviewAdapter(data);
+        reviewList.setAdapter(reviewAdapter);
     }
 }
--- app/src/main/java/com/song2/boostcourse/util/ReviewAdapter.java
+++ app/src/main/java/com/song2/boostcourse/util/ReviewAdapter.java
@@ -1,16 +1,31 @@
 package com.song2.boostcourse.util;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.song2.boostcourse.R;
 import com.song2.boostcourse.data.ReviewData;
+
 
 import java.util.ArrayList;
 
-public class ReviewAdapter extends BaseAdapter {
+import de.hdodenhof.circleimageview.CircleImageView;
 
-    ArrayList<ReviewData> ReviewList = new ArrayList<ReviewData>();
+public class ReviewAdapter extends BaseAdapter {
+    private LayoutInflater inflater = null;
+    private ArrayList<ReviewData> ReviewList;
+    private int nListCnt = 0;
+
+    public ReviewAdapter(ArrayList<ReviewData> _ReviewList){
+        ReviewList =_ReviewList;
+        nListCnt = _ReviewList.size();
+    }
 
     @Override
     public int getCount() {
@@ -29,6 +44,28 @@ public class ReviewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
-        return null;
+
+        Context context = viewGroup.getContext();
+
+        if (view == null) {
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        view = inflater.inflate(R.layout.item_review, viewGroup, false);
+
+        TextView user_id =  view.findViewById(R.id.tv_review_item_user_id);
+        TextView review_date = view.findViewById(R.id.tv_review_item_date);
+        TextView comment = view.findViewById(R.id.tv_review_item_contents);
+        TextView like_cnt = view.findViewById(R.id.tv_review_item_like_cnt);
+        CircleImageView profile_img = view.findViewById(R.id.cv_review_item_profile_img);
+        RatingBar rating = view.findViewById(R.id.rating_bar_review_item_review_rate);
+
+        //Glide.with(view).load(ReviewList.get(position).profile_img).into(profile_img);
+        rating.setRating(ReviewList.get(position).rate);
+        user_id.setText(ReviewList.get(position).user_id);
+        review_date.setText(ReviewList.get(position).date);
+        comment.setText(ReviewList.get(position).comment);
+        like_cnt.setText(ReviewList.get(position).like);
+
+        return view;
     }
 }
