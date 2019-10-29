e44b42b25107983ed51caa54463df848173d910e
seunghee63 <0603yang@naver.com>
2019-10-29 23:45:38 +0900

    Delete MultiTouch

--- app/src/main/java/com/song2/boostcourse/ui/gallery/GalleryActivity.java
+++ app/src/main/java/com/song2/boostcourse/ui/gallery/GalleryActivity.java
@@ -1,10 +1,7 @@
 package com.song2.boostcourse.ui.gallery;
 
-import android.annotation.SuppressLint;
-import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -15,9 +12,6 @@ import com.song2.boostcourse.R;
 public class GalleryActivity extends AppCompatActivity {
 
     static final String DETAILIMG = "DetailImg";
-
-    //new matrix 정보 저장 변수
-    private Matrix matrix = new Matrix();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,91 +29,5 @@ public class GalleryActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-        //아래 주석은 멀티터치를위한 터치 리스너 구현 부분입니다.
-/*
-        final ImageView iv = findViewById(R.id.photo_view_gallery_act_detailed_img);
-
-        Glide.with(getApplicationContext()).load(getIntent().getStringExtra(DETAILIMG)).into(pv);
-
-        iv.setScaleType(ImageView.ScaleType.MATRIX);
-
-        iv.setOnTouchListener(new View.OnTouchListener() {
-
-            float old_Pos = -1f; //첫번 째 터치
-            float new_Pos = -1f; //두번 째 터
-
-            public boolean onTouch (View v, MotionEvent event){
-
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
-                    case MotionEvent.ACTION_UP:
-
-                        old_Pos = -1f;
-
-                        new_Pos = -1f;
-
-                        break;
-
-                    //첫번째 터치가 일어나는 이벤트
-                    case MotionEvent.ACTION_DOWN:
-
-                        break;
-
-                    //두번째 터치가 일어나는 이벤트
-                    case MotionEvent.ACTION_POINTER_DOWN:
-
-                        float x = event.getX(0) - event.getX(1);
-                        float y = event.getY(0) - event.getY(1);
-
-                        old_Pos = (float) Math.sqrt(x * x + y * y);
-
-                        break;
-
-                    //두번째 터치를 떼면 일어나는 이벤트
-                    case MotionEvent.ACTION_POINTER_UP:
-
-                        old_Pos = -1f;
-                        new_Pos = -1f;
-
-                        break;
-
-                    //터치된 상태에서 손가락 움직이면 일어나는 이벤트
-                    case MotionEvent.ACTION_MOVE:
-
-                        if (event.getPointerCount() > 1) {
-
-                            float x_ = event.getX(0) - event.getX(1);
-                            float y_ = event.getY(0) - event.getY(1);
-
-                            new_Pos = (float) Math.sqrt(x_ * x_ + y_ * y_);
-
-                            float scale = new_Pos / old_Pos;
-                            matrix.postScale(scale, scale, 0, 0);
-
-
-                            float[] values = new float[9];
-                            matrix.getValues(values);
-
-
-                            if (values[0] < 0.1f) {
-
-                                scale = 0.1f;
-
-                                values[0] = scale;
-                                values[4] = scale;
-
-                                matrix.setValues(values);
-                                iv.setImageMatrix(matrix);
-
-                            }else {
-                                iv.setImageMatrix(matrix);
-                            }
-                        }
-                        break;
-                }
-                return GalleryActivity.super.onTouchEvent(event);
-            }
-        });*/
     }
 }
